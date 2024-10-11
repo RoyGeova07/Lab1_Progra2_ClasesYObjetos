56235a06ed6d22e6c99fac2c18e4e9474f89b2f0
Roy Umaña <royumana3@gmail.com>
2024-10-11 17:04:02 -0600

    Update Email.java

--- src/lab1_progra2_clasesyobjetos/Email.java
+++ src/lab1_progra2_clasesyobjetos/Email.java
@@ -2,26 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package labprogra2;
+package lab1_progra2_clasesyobjetos;
+
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author HP
  */
 public class Email {
-    
+
     //variables
     private String email;
     private String asunto;
     private String contenido;
     private boolean leido;
-    
+
     //constructor
-    public Email(String Email,String reason,String content){
-        email=Email;
-        asunto=reason;
-        contenido=content;
-        leido=false;
+    public Email(String Email, String reason, String content) {
+        email = Email;
+        asunto = reason;
+        contenido = content;
+        leido = false;
     }
 
     public String getEmail() {
@@ -39,16 +41,16 @@ public class Email {
     public boolean isLeido() {
         return leido;
     }
-    
-     public void leido(boolean read){
-         if(read==false){
-             leido=true;
-         }else if(read==true){
-             leido=false;
-         }
-     }
-     public void print(){
-         System.out.println("De: "+email+"\nAsunto: "+asunto+"\n"+contenido);
-     }
-    
+
+    public void leido(boolean read) {
+        if (read == false) {
+            leido = true;
+        } else if (read == true) {
+            leido = false;
+        }
+    }
+
+    public void print() {
+        System.out.println("De: " + email + "\nAsunto: " + asunto + "\n" + contenido);
+    }
 }
